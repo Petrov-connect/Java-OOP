93f64019024c5c7b5cd300bec2eeafa811d383a4
Petrov-connect <Petrov.connect@gmail.com>
2021-02-24 15:02:11 +0200

    java code

--- WorkingWithAbstraction/src/RhombusOfStars/Main.java
+++ WorkingWithAbstraction/src/RhombusOfStars/Main.java
@@ -1,4 +1,35 @@
 package RhombusOfStars;
+//created by J.M.
+
+import java.util.*;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        printRhombus(Integer.parseInt(scan.nextLine()));
+
+    }
+
+    static void printRhombus(int n) {
+        printHalfRhombus(n, n, true);
+        printHalfRhombus(n, n - 1, false);
+    }
+
+    private static void printHalfRhombus(int n, int end, boolean isTop) {
+        for (int i = 1; i <= end; i++) {
+            int spacesEnd = isTop ? n - i : i;
+            int starsEnd = isTop ? i : n - i;
+            printRow(spacesEnd, " ");
+            printRow(starsEnd, "* ");
+            System.out.println();
+        }
+    }
+
+    static void printRow(int end, String symbol) {
+        for (int j = 1; j <= end; j++) {
+            System.out.print(symbol);
+        }
+    }
 }
