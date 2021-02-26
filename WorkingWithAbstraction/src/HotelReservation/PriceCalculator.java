58318c36bae2af9ad4b2af7b3007909344932229
Petrov-connect <Petrov.connect@gmail.com>
2021-02-26 02:25:18 +0200

    java code

--- WorkingWithAbstraction/src/HotelReservation/PriceCalculator.java
+++ WorkingWithAbstraction/src/HotelReservation/PriceCalculator.java
@@ -8,7 +8,6 @@ public class PriceCalculator {
     private final Season season;
     private final DiscountType discountType;
 
-
     public PriceCalculator(double pricePerDay, int numberOfDays, Season season,DiscountType discountType) {
         this.pricePerDay = pricePerDay;
         this.numberOfDays = numberOfDays;
