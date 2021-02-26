3e0774c8c173df6d34768e0042645edc32bb0ef9
Petrov-connect <Petrov.connect@gmail.com>
2021-02-27 00:18:25 +0200

    java code

---
+++ WorkingWithAbstraction/src/cardRank/Main.java
@@ -0,0 +1,17 @@
+package cardRank;
+//created by J.M.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.nextLine()+":");
+        for (Ranks value : Ranks.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n",value.ordinal(),value.name());
+        }
+    }
+}
---
+++ WorkingWithAbstraction/src/cardRank/Ranks.java
@@ -0,0 +1,7 @@
+package cardRank;
+//created by J.M.
+
+public enum Ranks {
+
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
