ac7d248792e2777633058535b0ab260c1fa3d5bc
Petrov-connect <Petrov.connect@gmail.com>
2021-02-27 00:16:17 +0200

    java code

---
+++ WorkingWithAbstraction/src/trafficLights/TrafficState.java
@@ -0,0 +1,18 @@
+package trafficLights;
+
+public enum TrafficState {
+
+    RED, YELLOW, GREEN;
+
+    public static TrafficState update(TrafficState state) {
+        switch (state) {
+            case RED:
+            return GREEN;
+            case YELLOW:
+            return RED;
+            case GREEN:
+            return YELLOW;
+        }
+        return null;
+    }
+}
