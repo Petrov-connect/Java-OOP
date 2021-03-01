16ae8e52372c26f5c554ee66d653027bb28525d3
Petrov-connect <Petrov.connect@gmail.com>
2021-03-02 00:51:22 +0200

    java code

---
+++ Encapsulation/src/animalFarm/Main.java
@@ -0,0 +1,17 @@
+package animalFarm;
+//created by J.M.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        int age = Integer.parseInt(scan.nextLine());
+
+        Chicken chicken = new Chicken(name, age);
+        System.out.println(chicken.toString());
+
+    }
+}
