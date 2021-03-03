1a83b2da81c5c0e3402d289267b55904bbdcb6ce
Petrov-connect <Petrov.connect@gmail.com>
2021-03-03 16:47:39 +0200

    java code

--- Encapsulation/src/shoppingSpree/Person.java
+++ Encapsulation/src/shoppingSpree/Person.java
@@ -2,6 +2,7 @@ package shoppingSpree;
 //created by J.M.
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,25 +31,29 @@ public class Person {
         }
         this.money = money;
     }
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(this.products);
+    }
 
     public void buyProduct(Product product) {
-        if (this.money < product.getCost()) {
-            throw new IllegalArgumentException(String.format("%s can't afford %s", this.name, product.getName()));
+            if (this.money < product.getCost()) {
+                throw new IllegalArgumentException(String.format("%s can't afford %s", this.name, product.getName()));
+            }
+            this.money -= product.getCost();
+            this.products.add(product);
+            System.out.printf("%s bought %s%n", this.name, product.getName());
         }
-        this.money -= product.getCost();
-        this.products.add(product);
-        System.out.printf("%s bought %s%n", this.name, product.getName());
-    }
 
     public String getName() {
         return this.name;
     }
 
-    @Override
+   @Override
     public String toString() {
-        StringBuilder output = new StringBuilder(String.format("%s - ",this.name));
+        StringBuilder output = new StringBuilder(String.format("%s - ", this.name));
         output.append(this.products.isEmpty() ? "Nothing bought"
                 : output.append(products.stream().map(Product::getName).collect(Collectors.joining(", "))));
         return output.toString();
+
     }
 }
