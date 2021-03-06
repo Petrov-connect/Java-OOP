873602a819e49ed73b7426f03038656fc0fd3ad7
Petrov-connect <Petrov.connect@gmail.com>
2021-03-06 19:12:11 +0200

    java code

---
+++ InterfacesAndAbstraction/src/carShop/Car.java
@@ -0,0 +1,16 @@
+package carShop;
+//created by J.M.
+
+public interface Car {
+
+    Integer TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
