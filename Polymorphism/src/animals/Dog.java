0814acde9b627f43d94a437b89831353c221433d
Petrov-connect <Petrov.connect@gmail.com>
2021-03-10 16:52:28 +0200

    java code

---
+++ Polymorphism/src/animals/Dog.java
@@ -0,0 +1,13 @@
+package animals;
+//created by J.M.
+
+public class Dog extends Animal{
+    protected Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%s%nDJAAF",super.explainSelf());
+    }
+}
