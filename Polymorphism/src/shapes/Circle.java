e001e7d3da86f5cc297be4b55460bea026d265d8
Petrov-connect <Petrov.connect@gmail.com>
2021-03-10 16:51:22 +0200

    java code

---
+++ Polymorphism/src/shapes/Circle.java
@@ -0,0 +1,25 @@
+package shapes;
+//created by J.M.
+
+public class Circle extends Shape{
+
+   private final Double radius;
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public final Double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        return Math.PI*(2*getRadius());
+    }
+
+    @Override
+    public Double calculateArea() {
+        return Math.PI*Math.pow(getRadius(),2);
+    }
+}
