7ee16abbd4e4cf03276ea9766c3b37e71ca9557b
Petrov-connect <Petrov.connect@gmail.com>
2021-03-10 16:50:54 +0200

    java code

---
+++ Polymorphism/src/shapes/Shape.java
@@ -0,0 +1,20 @@
+package shapes;
+//created by J.M.
+
+public abstract class Shape {
+
+    private Double perimeter;
+    private Double area;
+
+    public abstract Double calculatePerimeter();
+
+    public abstract Double  calculateArea();
+
+    public Double getPerimeter() {
+        return perimeter;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+}
