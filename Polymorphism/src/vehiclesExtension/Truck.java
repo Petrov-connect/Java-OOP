be53916ecc036dbc7ea3f865d039d6b15ac20cb8
Petrov-connect <Petrov.connect@gmail.com>
2021-03-11 11:45:08 +0200

    java code

---
+++ Polymorphism/src/vehiclesExtension/Truck.java
@@ -0,0 +1,17 @@
+package vehiclesExtension;
+//created by J.M.
+
+public class Truck extends VehicleFunctionality {
+
+    private static final double INCREASE_FUEL_PER_KM = 1.6;
+    private static final double REDUCE_REFUEL = 0.95;
+
+    public Truck(double fuelQuantity, double fuelConsumption,double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + INCREASE_FUEL_PER_KM, tankCapacity);
+    }
+
+    @Override
+    public void refuel(double liters) {
+        super.refuel(liters * REDUCE_REFUEL);
+    }
+}
