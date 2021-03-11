4ff5494e1df6d8ba7d5250f3228d96415ec7849e
Petrov-connect <Petrov.connect@gmail.com>
2021-03-11 11:44:38 +0200

    java code

---
+++ Polymorphism/src/vehiclesExtension/Bus.java
@@ -0,0 +1,20 @@
+package vehiclesExtension;
+//created by J.M.
+
+public class Bus extends VehicleFunctionality{
+
+    private static final double INCREASED_FUEL_PER_KM = 1.4;
+    private static final double DECREASED_FUEL_PER_KM = -1.4;
+
+    protected Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + INCREASED_FUEL_PER_KM, tankCapacity);
+    }
+
+    public static double getIncreasedFuelPerKm() {
+        return INCREASED_FUEL_PER_KM;
+    }
+
+    public static double getDecreasedFuelPerKm() {
+        return DECREASED_FUEL_PER_KM;
+    }
+}
