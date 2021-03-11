d216a5fdfa4e64deb27027c613f26ebd0196db44
Petrov-connect <Petrov.connect@gmail.com>
2021-03-11 11:45:34 +0200

    java code

---
+++ Polymorphism/src/vehiclesExtension/Vehicles.java
@@ -0,0 +1,12 @@
+package vehiclesExtension;
+//created by J.M.
+
+public interface Vehicles {
+
+    void drive(double distance);
+
+    void drive(double distance,double decrease);
+
+    void refuel(double liters);
+
+}
