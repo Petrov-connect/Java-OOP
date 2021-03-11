65bfee0b774022732af4405593d0c7f0c9235812
Petrov-connect <Petrov.connect@gmail.com>
2021-03-11 03:01:45 +0200

    java code

---
+++ Polymorphism/src/vehicles/VehicleFactory.java
@@ -0,0 +1,28 @@
+package vehicles;
+//created by J.M.
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class VehicleFactory {
+
+    public static final Map<String, Vehicles> info = new HashMap<>();
+
+    public static void create(String[] info) {
+
+        switch (info[0]) {
+            case "Car":
+                VehicleFactory.info.put(info[0], new Car(Double.parseDouble(info[1]), Double.parseDouble(info[2])));
+                break;
+            case "Truck":
+                VehicleFactory.info.put(info[0], new Truck(Double.parseDouble(info[1]), Double.parseDouble(info[2])));
+                break;
+        }
+    }
+
+    public static Map<String, Vehicles> getInfo() {
+        return info;
+    }
+}
+
+
