ae56faeb0b67f6e8b4a345007c43acaacc633ef6
Petrov-connect <Petrov.connect@gmail.com>
2021-03-11 11:44:58 +0200

    java code

---
+++ Polymorphism/src/vehiclesExtension/Main.java
@@ -0,0 +1,18 @@
+package vehiclesExtension;
+//created by J.M.
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        IntStream.range(0, 3).mapToObj(i -> scan.nextLine().split("\\s+")).forEach(VehicleFactory::create);
+        IntStream.range(0, Integer.parseInt(scan.nextLine())).mapToObj(i -> scan.nextLine().split("\\s+"))
+                .forEach(VehicleFunctionality::execute);
+        VehicleFactory.getInfo().forEach((key, value) -> System.out.println(value.toString()));
+    }
+}
+
