c419e55b7ee0d17f684545abeae381c453479c75
Petrov-connect <Petrov.connect@gmail.com>
2021-03-16 16:52:27 +0200

    java code

---
+++ SOLID/src/loggerLib/appenders/SocketAppender.java
@@ -0,0 +1,44 @@
+package loggerLib.appenders;
+//created by J.M.
+
+import loggerLib.enumerations.ReportLevel;
+import loggerLib.layouts.interfaces.Layout;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketAppender extends AppenderImpl{
+
+    private Socket client;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public SocketAppender(Layout layout) throws IOException {
+        super(layout);
+        InetAddress address = InetAddress.getLocalHost();
+        this.startConnection("127.0.0.1",21);
+
+
+    }
+
+    public void startConnection(String ip, int port) throws IOException {
+        client = new Socket(ip, port);
+        out = new PrintWriter(client.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    @Override
+    public void append(String data, ReportLevel reportLevel, String message) throws IOException {
+        this.sendMessage(this.format(data,reportLevel,message));
+    }
+}
