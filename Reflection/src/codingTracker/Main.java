247430862469ec8a0dbf8d132de5e2fce4299afd
Petrov-connect <Petrov.connect@gmail.com>
2021-03-18 14:25:35 +0200

    java code

---
+++ Reflection/src/codingTracker/Main.java
@@ -0,0 +1,12 @@
+package codingTracker;
+//created by J.M.
+
+public class Main {
+
+    @Author(author = "George")
+    public static void main(String[] args) {
+
+        Tracker.printMethodsByAuthor(Tracker.class);
+
+    }
+}
