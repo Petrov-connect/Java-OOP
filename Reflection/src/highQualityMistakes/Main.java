e8b78128c9427d8a5047bcfe93f2905b0f00a1f5
Petrov-connect <Petrov.connect@gmail.com>
2021-03-18 14:24:02 +0200

    java code

---
+++ Reflection/src/highQualityMistakes/Main.java
@@ -0,0 +1,34 @@
+package highQualityMistakes;
+//created by J.M.
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class<Reflection>reflection = Reflection.class;
+
+        Arrays.stream(reflection.getDeclaredFields())
+                .filter(field->!Modifier.isPrivate(field.getModifiers()))
+                .sorted(Comparator.comparing(Field::getName))
+                .forEach(field->System.out.printf("%s must be private!%n",field.getName()));
+
+        Arrays.stream(reflection.getDeclaredMethods())
+                .filter(current->current.getName().startsWith("get"))
+                .sorted(Comparator.comparing(Method::getName))
+                .filter(getter->!Modifier.isPublic(getter.getModifiers()))
+                .forEach(getter -> System.out.printf("%s have to be public!%n", getter.getName()));
+
+        Arrays.stream(reflection.getDeclaredMethods())
+                .filter(current->current.getName().startsWith("set"))
+                .sorted(Comparator.comparing(Method::getName))
+                .filter(setter->!Modifier.isPrivate(setter.getModifiers()))
+                .forEach(setter -> System.out.printf("%s have to be private!%n", setter.getName()));
+
+    }
+}
