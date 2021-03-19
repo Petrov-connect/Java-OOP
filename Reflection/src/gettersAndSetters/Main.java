9cb3f027aa74b982a76d63e57afb0c4329821f11
Petrov-connect <Petrov.connect@gmail.com>
2021-03-19 13:36:05 +0200

    java code

--- Reflection/src/gettersAndSetters/Main.java
+++ Reflection/src/gettersAndSetters/Main.java
@@ -9,15 +9,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Class<Reflection>reflection = Reflection.class;
-
-        Arrays.stream(reflection.getDeclaredMethods())
+        Arrays.stream(Reflection.class.getDeclaredMethods())
                 .filter(current->current.getName().startsWith("get"))
                 .sorted(Comparator.comparing(Method::getName))
                 .forEach(getter -> System.out.printf("%s will return class %s%n"
                         ,getter.getName(),getter.getReturnType().getName()));
 
-        Arrays.stream(reflection.getDeclaredMethods())
+        Arrays.stream(Reflection.class.getDeclaredMethods())
                 .filter(current->current.getName().startsWith("set"))
                 .sorted(Comparator.comparing(Method::getName))
                 .forEach(setter -> System.out.printf("%s and will set field of class %s%n"
