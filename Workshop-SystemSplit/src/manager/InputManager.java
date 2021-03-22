8a504b0cd4bd34d579cfaac16b62f8207a415f60
Petrov-connect <Petrov.connect@gmail.com>
2021-03-22 17:11:39 +0200

    java code

---
+++ Workshop-SystemSplit/src/manager/InputManager.java
@@ -0,0 +1,27 @@
+package manager;
+//created by J.M.
+
+import interfaces.InputReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputManager implements InputReader {
+    private final BufferedReader read;
+
+    public InputManager() {
+        this.read = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    @Override
+    public String readLine() {
+        String result = null;
+        try {
+            result = this.read.readLine().trim();
+        } catch (IOException ignored) {
+
+        }
+        return result;
+    }
+}
