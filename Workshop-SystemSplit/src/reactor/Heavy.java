53850ae22f3d56a4abf556a2f785cbabd60909dc
Petrov-connect <Petrov.connect@gmail.com>
2021-03-22 17:09:46 +0200

    java code

---
+++ Workshop-SystemSplit/src/reactor/Heavy.java
@@ -0,0 +1,20 @@
+package reactor;
+//created by J.M.
+
+public class Heavy extends ReactorImpl {
+
+    public Heavy(String name, int capacity, int memory) {
+
+        super(name, increaseCapacity(capacity), decreaseMemory(memory));
+    }
+
+    private static int increaseCapacity(int capacity) {
+        // doubles capacity
+        return capacity * 2;
+    }
+
+    private static int decreaseMemory(int memory) {
+        // decreases Memory by 25%
+        return memory - (memory / 4);
+    }
+}
