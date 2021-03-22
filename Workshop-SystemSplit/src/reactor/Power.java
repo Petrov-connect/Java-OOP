02fbcc16a2613566bd32dd12c02cc343133b9bab
Petrov-connect <Petrov.connect@gmail.com>
2021-03-22 17:10:05 +0200

    java code

---
+++ Workshop-SystemSplit/src/reactor/Power.java
@@ -0,0 +1,19 @@
+package reactor;
+//created by J.M.
+
+public class Power extends ReactorImpl {
+
+    public Power(String name, int capacity, int memory) {
+        super(name, decreaseCapacity(capacity), increaseMemory(memory));
+    }
+
+    private static int decreaseCapacity(int capacity) {
+        //decreases capacity by 75%
+        return capacity - ((capacity * 3) / 4);
+    }
+
+    private static int increaseMemory(int memory) {
+        //increases memory by 75%
+        return memory + ((memory * 3) / 4);
+    }
+}
