75f5e74ce0fd404e6dbc012966a58f4510b05032
Petrov-connect <Petrov.connect@gmail.com>
2021-03-22 17:06:34 +0200

    java code

--- Workshop-SystemSplit/src/Main.java
+++ Workshop-SystemSplit/src/Main.java
@@ -1,7 +1,12 @@
+//created by J.M.
+
+import manager.MyManager;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        MyManager manager = new MyManager();
+        manager.run();
     }
 }
