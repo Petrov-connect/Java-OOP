3cfa0387749ca358207d873d198e69b5d121c80a
Petrov-connect <Petrov.connect@gmail.com>
2021-03-23 16:25:07 +0200

    java code

---
+++ Workshop-SystemSplit/src/interfaces/Repository.java
@@ -0,0 +1,17 @@
+package interfaces;
+//created by J.M.
+
+import interfaces.Reactor;
+
+import java.util.Map;
+
+public interface Repository {
+
+    Map<String, Reactor> getRepository();
+
+    void release(String hardware ,String software);
+
+    String analyze();
+
+    String finalizes();
+}
