0f323b9774c3958903990eba1a8d1b9a8b878683
Petrov-connect <Petrov.connect@gmail.com>
2021-03-23 16:24:57 +0200

    java code

---
+++ Workshop-SystemSplit/src/interfaces/Reactor.java
@@ -0,0 +1,26 @@
+package interfaces;
+//created by J.M.
+
+import software.SoftwareImpl;
+
+import java.util.List;
+
+public interface Reactor {
+
+    List<SoftwareImpl> getSoftware();
+
+    void add(SoftwareImpl software);
+
+    void remove(SoftwareImpl software);
+
+    String getName();
+
+    int getMaximumCapacity();
+
+    int getMaximumMemory();
+
+    int memoryUsed();
+
+    int capacityUsed();
+
+}
