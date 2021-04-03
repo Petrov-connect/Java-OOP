e534caa9e7b289dd7e8ae538950d725bdd049706
Petrov-connect <Petrov.connect@gmail.com>
2021-04-03 16:02:41 +0300

    java code

--- Exam/src/bakery/repositories/DrinkRepositoryImpl.java
+++ Exam/src/bakery/repositories/DrinkRepositoryImpl.java
@@ -1,12 +1,16 @@
-package bakery.repositories.interfaces;
+package bakery.repositories;
+//created by J.M.
 
 import bakery.entities.drinks.interfaces.Drink;
+import bakery.repositories.interfaces.DrinkRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class DrinkRepositoryImpl implements DrinkRepository<Drink> {
-    private Collection<Drink> models;
+
+    private final Collection<Drink> models;
 
     public DrinkRepositoryImpl() {
         this.models = new ArrayList<>();
@@ -15,16 +19,21 @@ public class DrinkRepositoryImpl implements DrinkRepository<Drink> {
 
     @Override
     public Drink getByNameAndBrand(String drinkName, String drinkBrand) {
-        return null;
+
+        return this.models.stream().filter(e -> e.getName().equals(drinkName)
+                && e.getBrand().equals(drinkBrand)).findFirst().orElse(null);
     }
 
     @Override
     public Collection<Drink> getAll() {
-        return null;
+
+        return Collections.unmodifiableCollection(this.models);
     }
 
     @Override
     public void add(Drink drink) {
+
+        this.models.add(drink);
 
     }
 }
