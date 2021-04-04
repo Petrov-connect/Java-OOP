3295f73df48426eab5a9f6621dbe1784aa29bfd6
Petrov-connect <Petrov.connect@gmail.com>
2021-04-04 18:23:45 +0300

    java code

--- Exam-Preparation-2/src/cresla/Main.java
+++ Exam-Preparation-2/src/cresla/Main.java
@@ -1,15 +1,47 @@
 package cresla;
 //created by J.M.
 
-import cresla.manager.InputReaderImpl;
-import cresla.manager.ManagerImpl;
-import cresla.manager.OutputWriterImpl;
+import cresla.core.InputReaderImpl;
+import cresla.core.ManagerImpl;
+import cresla.core.OutputWriterImpl;
+import cresla.interfaces.InputReader;
+import cresla.interfaces.Manager;
+import cresla.interfaces.OutputWriter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        InputReader reader = new InputReaderImpl();
+        OutputWriter writer = new OutputWriterImpl();
+        Manager manager = new ManagerImpl();
 
-        ManagerImpl manager = new ManagerImpl(new InputReaderImpl(), new OutputWriterImpl());
-        manager.run();
+        while (true) {
+
+            String[] command = reader.readLine().split("\\s+");
+
+            List<String> arguments = Arrays.stream(command).skip(1).collect(Collectors.toList());
+
+            switch (command[0]) {
+
+                case "Reactor":
+                    writer.write(manager.reactorCommand(arguments));
+                    break;
+                case "Module":
+                    writer.write(manager.moduleCommand(arguments));
+                    break;
+                case "Report":
+                    writer.write(manager.reportCommand(arguments));
+                    break;
+                case "Exit":
+                    writer.write(manager.exitCommand(arguments));
+                    return;
+            }
+        }
     }
 }
+
