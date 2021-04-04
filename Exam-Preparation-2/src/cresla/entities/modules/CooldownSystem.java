43d56d1d18dfc46d82de85e62ebbb4c3bfe9ea89
Petrov-connect <Petrov.connect@gmail.com>
2021-04-04 18:25:47 +0300

    java code

--- Exam-Preparation-2/src/cresla/entities/modules/CooldownSystem.java
+++ Exam-Preparation-2/src/cresla/entities/modules/CooldownSystem.java
@@ -1,10 +1,9 @@
-package cresla.modules;
+package cresla.entities.modules;
 //created by J.M.
 
-public class CooldownSystem extends AbsorbingModules {
+public class CooldownSystem extends AbsorbingModules{
 
     public CooldownSystem(int id, int heatAbsorbing) {
         super(id, heatAbsorbing);
     }
-
 }
