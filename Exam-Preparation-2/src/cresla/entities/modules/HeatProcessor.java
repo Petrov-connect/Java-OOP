03b68639cb96ee5d51a5f33ea97cc156b0379130
Petrov-connect <Petrov.connect@gmail.com>
2021-04-04 18:25:04 +0300

    java code

---
+++ Exam-Preparation-2/src/cresla/entities/modules/HeatProcessor.java
@@ -0,0 +1,10 @@
+package cresla.entities.modules;
+//created by J.M.
+
+public class HeatProcessor extends AbsorbingModules{
+
+    public HeatProcessor(int id, int heatAbsorbing) {
+
+        super(id, heatAbsorbing);
+    }
+}
