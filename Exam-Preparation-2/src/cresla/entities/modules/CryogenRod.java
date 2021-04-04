7856f170545fbc10f0d17e8c8cd66fe0d70484c5
Petrov-connect <Petrov.connect@gmail.com>
2021-04-04 18:25:28 +0300

    java code

---
+++ Exam-Preparation-2/src/cresla/entities/modules/CryogenRod.java
@@ -0,0 +1,10 @@
+package cresla.entities.modules;
+//created by J.M.
+
+public class CryogenRod extends EnergyModules{
+
+
+    public CryogenRod(int id, int energyOutput) {
+        super(id, energyOutput);
+    }
+}
