fa76375d376b77e37ca24a7f70eb431571a5d58a
Petrov-connect <Petrov.connect@gmail.com>
2021-04-05 00:26:47 +0300

    java code

--- Encapsulation/src/salaryIncrease/Main.java
+++ Encapsulation/src/salaryIncrease/Main.java
@@ -12,9 +12,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
+
+        int num = Integer.parseInt(reader.readLine());
         List<Person> people = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num; i++) {
             String[] input = reader.readLine().split(" ");
             people.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
         }
