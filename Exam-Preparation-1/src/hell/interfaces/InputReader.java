527e27056d21cbf3fc6101d80ad242852e92e0d0
Petrov-connect <Petrov.connect@gmail.com>
2021-04-06 15:18:22 +0300

    java code

--- Exam-Preparation-1/src/hell/interfaces/InputReader.java
+++ Exam-Preparation-1/src/hell/interfaces/InputReader.java
@@ -1,9 +1,10 @@
 package hell.interfaces;
+//created by J.M.
 
 /**
  *  This is the interface for the InputReader
  *  @method String readLine() - a method which is made to return a read line from the corresponding Stream.
  */
 public interface InputReader {
-    String readLine();
+    String readLine() ;
 }
