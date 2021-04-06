49d854bec7a15541a6818b9233fed0f5613a8ed0
Petrov-connect <Petrov.connect@gmail.com>
2021-04-06 18:11:20 +0300

    java code

--- Exam/src/easterRaces/core/ControllerImpl.java
+++ Exam/src/easterRaces/core/ControllerImpl.java
@@ -36,57 +36,73 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String createDriver(String driverName) {
+
         Driver newDriver = new DriverImpl(driverName);
-        if (driverRepository.getByName(driverName)!=null) {
+
+        if (driverRepository.getByName(driverName) != null) {
             throw new IllegalArgumentException(String.format(DRIVER_EXISTS, driverName));
         }
         driverRepository.add(newDriver);
+
         return String.format(DRIVER_CREATED, driverName);
     }
 
     @Override
     public String createCar(String type, String model, int horsePower) {
+
         Car car = null;
+
         if (type.equals("Muscle")) {
-           car = new MuscleCar(model, horsePower);
+            car = new MuscleCar(model, horsePower);
         } else if (type.equals("Sports")) {
             car = new SportsCar(model, horsePower);
         }
-        if (carRepository.getByName(model)!=null) {
+        if (carRepository.getByName(model) != null) {
             throw new IllegalArgumentException(String.format(CAR_EXISTS, model));
         } else {
             carRepository.add(car);
         }
         assert car != null;
-        return String.format(CAR_CREATED,car.getClass().getSimpleName(), model);
+
+        return String.format(CAR_CREATED, car.getClass().getSimpleName(), model);
     }
 
     @Override
     public String addCarToDriver(String driverName, String carModel) {
+
         if (driverRepository.getByName(driverName) == null) {
+
             throw new IllegalArgumentException(String.format(DRIVER_NOT_FOUND, driverName));
-        } else if ( carRepository.getByName(carModel) == null) {
-            throw new IllegalArgumentException(String.format(CAR_NOT_FOUND, carModel));
-        } else {
-            driverRepository.getByName(driverName).addCar( carRepository.getByName(carModel));
-            return String.format(CAR_ADDED, driverName, carModel);
         }
+        if (carRepository.getByName(carModel) == null) {
+
+            throw new IllegalArgumentException(String.format(CAR_NOT_FOUND, carModel));
+        }
+
+        driverRepository.getByName(driverName).addCar(carRepository.getByName(carModel));
+
+        return String.format(CAR_ADDED, driverName, carModel);
     }
 
     @Override
     public String addDriverToRace(String raceName, String driverName) {
+
         if (driverRepository.getByName(driverName) == null) {
             throw new IllegalArgumentException(String.format(DRIVER_NOT_FOUND, driverName));
+
         } else if (raceRepository.getByName(raceName) == null) {
+
             throw new IllegalArgumentException(String.format(RACE_NOT_FOUND, raceName));
-        } else {
-            raceRepository.getByName(raceName).addDriver(driverRepository.getByName(driverName));
-            return String.format(DRIVER_ADDED, driverName, raceName);
         }
+        raceRepository.getByName(raceName).addDriver(driverRepository.getByName(driverName));
+
+        return String.format(DRIVER_ADDED, driverName, raceName);
+
     }
 
     @Override
     public String startRace(String raceName) {
+
         if (raceRepository.getByName(raceName) == null) {
             throw new IllegalArgumentException(String.format(RACE_NOT_FOUND, raceName));
         }
@@ -98,9 +114,12 @@ public class ControllerImpl implements Controller {
         if (drivers.size() < 3) {
             throw new IllegalArgumentException(String.format(RACE_INVALID, raceName, 3));
         }
+
         StringBuilder output = new StringBuilder();
+
         drivers.entrySet().stream().sorted((first, second) ->
                 second.getValue().compareTo(first.getValue())).limit(3).forEach(entry -> {
+
             switch (place) {
                 case "First":
                     output.append(String.format(DRIVER_FIRST_POSITION, entry.getKey(), raceName));
@@ -118,16 +137,20 @@ public class ControllerImpl implements Controller {
                     break;
             }
         });
+
         return output.toString().trim();
     }
 
     @Override
     public String createRace(String name, int laps) {
+
         Race race = new RaceImpl(name, laps);
+
         if (raceRepository.getAll().contains(race)) {
             throw new IllegalArgumentException(String.format(RACE_EXISTS, name));
         }
         raceRepository.add(race);
+
         return String.format(RACE_CREATED, name);
     }
 }
