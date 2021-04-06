2c4d5d525e7a7ab58a72a2fa6922878ed6a1324c
Petrov-connect <Petrov.connect@gmail.com>
2021-04-07 00:24:11 +0300

    java code

---
+++ Exam/src/onlineShop/models/Product.java
@@ -0,0 +1,14 @@
+package onlineShop.models;
+//created by J.M.
+
+public interface Product {
+    int getId();
+
+    String getManufacturer();
+
+    String getModel();
+
+    double getPrice();
+
+    double getOverallPerformance();
+}
