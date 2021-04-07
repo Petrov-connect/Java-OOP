f70520b696d6c26ca15c67a7228eaa3076f853c0
Petrov-connect <Petrov.connect@gmail.com>
2021-04-08 01:38:57 +0300

    java  code

--- Exam/src/onlineShop/models/products/peripherals/Keyboard.java
+++ Exam/src/onlineShop/models/products/peripherals/Keyboard.java
@@ -3,7 +3,13 @@ package onlineShop.models.products.peripherals;
 
 public class Keyboard extends BasePeripheral{
 
-    public Keyboard(int id, String manufacturer, String model, double price, double overallPerformance, String connectionType) {
+    public Keyboard(int id,
+                    String manufacturer,
+                    String model,
+                    double price,
+                    double overallPerformance,
+                    String connectionType) {
+
         super(id, manufacturer, model, price, overallPerformance, connectionType);
     }
 }
