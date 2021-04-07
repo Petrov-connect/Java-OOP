f39be181c33cca877f4c74fbec2c4c87843f5daf
Petrov-connect <Petrov.connect@gmail.com>
2021-04-08 01:39:33 +0300

    java  code

--- Exam/src/onlineShop/models/products/computers/Laptop.java
+++ Exam/src/onlineShop/models/products/computers/Laptop.java
@@ -3,9 +3,10 @@ package onlineShop.models.products.computers;
 
 public class Laptop extends BaseComputer{
 
-    private static double overallPerformance = 10;
+    private static double DEFAULT_PERFORMANCE = 10;
 
     public Laptop(int id, String manufacturer, String model, double price) {
-        super(id, manufacturer, model, price, overallPerformance);
+
+        super(id, manufacturer, model, price, DEFAULT_PERFORMANCE);
     }
 }
