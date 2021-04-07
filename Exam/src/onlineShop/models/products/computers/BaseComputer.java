72f476552df82d10c90468720b07563245979135
Petrov-connect <Petrov.connect@gmail.com>
2021-04-08 01:39:58 +0300

    java  code

--- Exam/src/onlineShop/models/products/computers/BaseComputer.java
+++ Exam/src/onlineShop/models/products/computers/BaseComputer.java
@@ -2,26 +2,27 @@ package onlineShop.models.products.computers;
 //created by J.M.
 
 import onlineShop.models.BaseProduct;
-import onlineShop.models.Product;
 import onlineShop.models.products.components.Component;
 import onlineShop.models.products.peripherals.Peripheral;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static onlineShop.common.constants.ExceptionMessages.*;
+import static onlineShop.common.constants.OutputMessages.COMPUTER_COMPONENTS_TO_STRING;
+import static onlineShop.common.constants.OutputMessages.COMPUTER_PERIPHERALS_TO_STRING;
 
-public abstract class BaseComputer extends BaseProduct {
+
+public abstract class BaseComputer extends BaseProduct implements Computer {
 
     private List<Component> components;
     private List<Peripheral> peripherals;
 
     protected BaseComputer(int id,
-                        String manufacturer,
-                        String model,
-                        double price,
-                        double overallPerformance) {
+                           String manufacturer,
+                           String model,
+                           double price,
+                           double overallPerformance) {
 
         super(id, manufacturer, model, price, overallPerformance);
 
@@ -29,30 +30,41 @@ public abstract class BaseComputer extends BaseProduct {
         this.peripherals = new LinkedList<>();
     }
 
-    public List<Component> getComponents() {
-        return components;
+    @Override
+    public double getOverallPerformance() {
+
+        return this.components.isEmpty() ? super.getOverallPerformance()
+                : super.getOverallPerformance() + getComponentsAveragePerformance();
+
     }
 
-    public List<Peripheral> getPeripherals() {
-        return peripherals;
+    @Override
+    public double getPrice() {
+
+        return super.getPrice() +
+                components.stream().mapToDouble(Component::getPrice).sum() +
+                peripherals.stream().mapToDouble(Peripheral::getPrice).sum();
     }
 
+    @Override
     public void addComponent(Component component) {
 
-        Component exist = components.stream().filter(e -> e.getClass().getSimpleName()
-                .equals(component.getClass().getSimpleName())).findFirst().orElse(null);
-        if (exist != null) {
+        if (components.stream().anyMatch(e -> e.getClass().getSimpleName()
+                .equals(component.getClass().getSimpleName()))) {
+
             throw new IllegalArgumentException(String.format(EXISTING_COMPONENT,
-                    exist.getClass().getSimpleName(), this.getClass().getSimpleName(), this.getId()));
+                    component.getClass().getSimpleName(), this.getClass().getSimpleName(), this.getId()));
         }
         this.components.add(component);
     }
 
+    @Override
     public Component removeComponent(String componentType) {
 
         Component exist = components.stream().filter(e -> e.getClass().getSimpleName()
                 .equals(componentType)).findFirst().orElse(null);
-        if(components.isEmpty() || exist == null){
+
+        if (exist == null) {
             throw new IllegalArgumentException(String.format(NOT_EXISTING_COMPONENT,
                     componentType, this.getClass().getSimpleName(), this.getId()));
         }
@@ -60,45 +72,31 @@ public abstract class BaseComputer extends BaseProduct {
         return exist;
     }
 
+    @Override
     public void addPeripheral(Peripheral peripheral) {
 
-        Peripheral exist = peripherals.stream().filter(e -> e.getClass().getSimpleName()
-                .equals(peripheral.getClass().getSimpleName())).findFirst().orElse(null);
-        if (exist != null) {
+        if (peripherals.stream().anyMatch(c -> c.getClass().getSimpleName()
+                .equals(peripheral.getClass().getSimpleName()))) {
+
             throw new IllegalArgumentException(String.format(EXISTING_PERIPHERAL,
-                    exist.getClass().getSimpleName(), this.getClass().getSimpleName(), this.getId()));
+                    peripheral.getClass().getSimpleName(), this.getClass().getSimpleName(), this.getId()));
         }
         this.peripherals.add(peripheral);
     }
 
+    @Override
     public Peripheral removePeripheral(String peripheralType) {
 
         Peripheral exist = peripherals.stream().filter(e -> e.getClass().getSimpleName()
                 .equals(peripheralType)).findFirst().orElse(null);
-        if(peripherals.isEmpty() || exist == null){
-            throw new IllegalArgumentException(String.format(NOT_EXISTING_COMPONENT,
+
+        if (exist == null) {
+            throw new IllegalArgumentException(String.format(NOT_EXISTING_PERIPHERAL,
                     peripheralType, this.getClass().getSimpleName(), this.getId()));
         }
         peripherals.remove(exist);
+
         return exist;
-    }
-
-    @Override
-    public double getPrice() {
-
-        return super.getPrice() +
-                components.stream().mapToDouble(Product::getPrice).sum() +
-                peripherals.stream().mapToDouble(Product::getPrice).sum();
-    }
-
-    @Override
-    public double getOverallPerformance() {
-
-        if (this.components.isEmpty()) {
-            return super.getOverallPerformance();
-        } else {
-            return getAveragePerformance();
-        }
     }
 
     @Override
@@ -106,27 +104,43 @@ public abstract class BaseComputer extends BaseProduct {
 
         StringBuilder out = new StringBuilder(super.toString());
 
-        out.append(System.lineSeparator())
-                .append(String.format(" Components (%d):", this.components.size()))
+        out.append(System.lineSeparator()).append(" ")
+                .append(String.format(COMPUTER_COMPONENTS_TO_STRING, this.components.size()))
                 .append(System.lineSeparator());
         if (!this.components.isEmpty()) {
-            out.append(" ")
-                    .append(String.format("%s", this.components.stream().map(String::valueOf)
-                            .collect(Collectors.joining(" %n"))));
+            components.forEach(component -> out.append("  ").append(component).append(System.lineSeparator()));
         }
-        out.append(String.format(" Peripherals (%d); Average Overall Performance (%f):"
-                , this.peripherals.size(), getOverallPerformance()))
+        out.append(" ")
+                .append(String.format(COMPUTER_PERIPHERALS_TO_STRING
+                        , this.peripherals.size(), getPeripheralsAveragePerformance()))
                 .append(System.lineSeparator());
         if (!this.peripherals.isEmpty()) {
-            out.append(" ")
-                    .append(String.format("%s", this.peripherals.stream().map(String::valueOf)
-                            .collect(Collectors.joining(" %n"))));
+            peripherals.forEach(peripheral -> out.append("  ").append(peripheral).append(System.lineSeparator()));
         }
         return out.toString().trim();
     }
 
-    private double getAveragePerformance() {
+    @Override
+    public List<Component> getComponents() {
 
-        return this.peripherals.stream().mapToDouble(Product::getOverallPerformance).average().orElse(0);
+        return this.components;
+    }
+
+    @Override
+    public List<Peripheral> getPeripherals() {
+
+        return this.peripherals;
+    }
+
+    private double getPeripheralsAveragePerformance() {
+
+        return this.getPeripherals().stream()
+                .mapToDouble(Peripheral::getOverallPerformance).average().orElse(0);
+    }
+
+    private double getComponentsAveragePerformance() {
+
+        return this.getComponents().stream()
+                .mapToDouble(Component::getOverallPerformance).average().orElse(0);
     }
 }
