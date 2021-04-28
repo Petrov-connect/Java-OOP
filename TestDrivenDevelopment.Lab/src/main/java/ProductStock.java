5436b3ca8e50587a1de4ad048f51305e85311492
Petrov-connect <Petrov.connect@gmail.com>
2021-04-28 13:04:15 +0300

    java code

--- TestDrivenDevelopment.Lab/src/main/java/ProductStock.java
+++ TestDrivenDevelopment.Lab/src/main/java/ProductStock.java
@@ -1,24 +1,26 @@
-package main.java;
+//created by J.M.
 
 public interface ProductStock extends Iterable<Product> {
-    //getter
-    int getCount();
 
-    //Validations
-    boolean contains(Product product);
+    Integer getCount();
 
-    //Modifications
+    Boolean contains(Product product);
+
     void add(Product product);
+
     void changeQuantity(String product, int quantity);
 
-    //Retrievals
     Product find(int index);
+
     Product findByLabel(String label);
+
     Iterable<Product> findFirstByAlphabeticalOrder(int count);
 
-    //Querying
     Iterable<Product> findAllInRange(double lo, double hi);
+
     Iterable<Product> findAllByPrice(double price);
+
     Iterable<Product> findFirstMostExpensiveProducts(int count);
+
     Iterable<Product> findAllByQuantity(int quantity);
 }
